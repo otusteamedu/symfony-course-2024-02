2721e07209a8c7de86bd1b0c586162742a4b615d
Mikhail Kamorin <m.v.kamorin@gmail.com>
2023-04-11 17:53:46 +0300

    Add cache on app level

--- src/Manager/TweetManager.php
+++ src/Manager/TweetManager.php
@@ -6,10 +6,14 @@ use App\Entity\Tweet;
 use App\Entity\User;
 use App\Repository\TweetRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Cache\CacheItemPoolInterface;
 
 class TweetManager
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly CacheItemPoolInterface $cacheItemPool,
+    )
     {
     }
 
@@ -30,9 +34,16 @@ class TweetManager
      */
     public function getTweets(int $page, int $perPage): array
     {
-        /** @var TweetRepository $TweetRepository */
+        /** @var TweetRepository $tweetRepository */
         $tweetRepository = $this->entityManager->getRepository(Tweet::class);
 
-        return $tweetRepository->getTweets($page, $perPage);
+        $tweetsItem = $this->cacheItemPool->getItem("tweets_{$page}_{$perPage}");
+        if (!$tweetsItem->isHit()) {
+            $tweets = $tweetRepository->getTweets($page, $perPage);
+            $tweetsItem->set(array_map(static fn(Tweet $tweet) => $tweet->toArray(), $tweets));
+            $this->cacheItemPool->save($tweetsItem);
+        }
+
+        return $tweetsItem->get();
     }
 }
