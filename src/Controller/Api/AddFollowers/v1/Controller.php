7b02d45aca347fcf93cf89c814723c8edf3b04ef
Mikhail Kamorin <m.v.kamorin@gmail.com>
2023-09-04 16:35:28 +0300

    Send message via Symfony Messenger

--- src/Controller/Api/AddFollowers/v1/Controller.php
+++ src/Controller/Api/AddFollowers/v1/Controller.php
@@ -3,28 +3,29 @@
 namespace App\Controller\Api\AddFollowers\v1;
 
 use App\Manager\UserManager;
-use App\Service\AsyncService;
 use App\Service\SubscriptionService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\RequestParam;
-use JsonException;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class Controller extends AbstractFOSRestController
 {
-    public function __construct(
-        private readonly SubscriptionService $subscriptionService,
-        private readonly UserManager $userManager,
-        private readonly AsyncService $asyncService,
-    )
+    private SubscriptionService $subscriptionService;
+
+    private UserManager $userManager;
+
+    private MessageBusInterface $messageBus;
+
+    public function __construct(SubscriptionService $subscriptionService, UserManager $userManager, MessageBusInterface $messageBus)
     {
+        $this->subscriptionService = $subscriptionService;
+        $this->userManager = $userManager;
+        $this->messageBus = $messageBus;
     }
 
-    /**
-     * @throws JsonException
-     */
-    #[Route(path: '/api/v1/add-followers', methods: ['POST'])]
+    #[Rest\Post(path: '/api/v1/add-followers')]
     #[RequestParam(name: 'userId', requirements: '\d+')]
     #[RequestParam(name: 'followersLogin')]
     #[RequestParam(name: 'count', requirements: '\d+')]
@@ -37,9 +38,10 @@ class Controller extends AbstractFOSRestController
                 $createdFollowers = $this->subscriptionService->addFollowers($user, $followersLogin, $count);
                 $view = $this->view(['created' => $createdFollowers], 200);
             } else {
-                $message = $this->subscriptionService->getFollowersMessages($user, $followersLogin, $count);
-                $result = $this->asyncService->publishMultipleToExchange(AsyncService::ADD_FOLLOWER, $message);
-                $view = $this->view(['success' => $result], $result ? 200 : 500);
+                for ($i = 0; $i < $count; $i++) {
+                    $this->messageBus->dispatch(new AddFollowersDTO($user->getId(), "$followersLogin #$i", 1));
+                }
+                $view = $this->view(['success' => true], 200);
             }
         } else {
             $view = $this->view(['success' => false], 404);
