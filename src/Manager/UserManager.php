f69685fc2736deb575e6dede5f12f57394ab1988
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-03-15 16:55:52 +0300

    Fix issue by clearing EntityManager

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -22,4 +22,17 @@ class UserManager
 
         return $user;
     }
+
+    public function clearEntityManager(): void
+    {
+        $this->entityManager->clear();
+    }
+
+    public function findUser(int $id): ?User
+    {
+        $repository = $this->entityManager->getRepository(User::class);
+        $user = $repository->find($id);
+
+        return $user instanceof User ? $user : null;
+    }
 }
