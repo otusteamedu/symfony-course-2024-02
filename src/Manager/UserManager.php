744ce01791f9fc455856ccc4c086ad6107195f15
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-03-15 16:51:24 +0300

    Save user to database

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -3,15 +3,22 @@
 namespace App\Manager;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserManager
 {
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {
+    }
+
     public function create(string $login): User
     {
         $user = new User();
         $user->setLogin($login);
         $user->setCreatedAt();
         $user->setUpdatedAt();
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
 
         return $user;
     }
