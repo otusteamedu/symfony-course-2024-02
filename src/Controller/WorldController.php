e72e9ce53c6a40bf7fbab9876ba0554b7f8447e1
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-03-15 17:04:36 +0300

    Add search by login

--- src/Controller/WorldController.php
+++ src/Controller/WorldController.php
@@ -3,22 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Service\UserBuilderService;
+use App\Manager\UserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class WorldController extends AbstractController
 {
-    public function __construct(private readonly UserBuilderService $userBuilderService)
+    public function __construct(private readonly UserManager $userManager)
     {
     }
 
     public function hello(): Response
     {
-        $users = $this->userBuilderService->createUserWithFollower(
-            'J.R.R. Tolkien',
-            'Ivan Ivanov'
-        );
+        $users = $this->userManager->findUsersByLogin('Ivan Ivanov');
 
         return $this->json(array_map(static fn(User $user) => $user->toArray(), $users));
     }
--- src/Service/UserBuilderService.php
+++ src/Service/UserBuilderService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Entity\User;
+use App\Manager\SubscriptionManager;
 use App\Manager\TweetManager;
 use App\Manager\UserManager;
 
@@ -11,6 +12,7 @@ class UserBuilderService
     public function __construct(
         private readonly TweetManager $tweetManager,
         private readonly UserManager $userManager,
+        private readonly SubscriptionManager $subscriptionManager,
     ) {
     }
 
@@ -35,6 +37,7 @@ class UserBuilderService
         $user = $this->userManager->create($login);
         $follower = $this->userManager->create($followerLogin);
         $this->userManager->subscribeUser($user, $follower);
+        $this->subscriptionManager->addSubscription($user, $follower);
 
         return [$user, $follower];
     }
