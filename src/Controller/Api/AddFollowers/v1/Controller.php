otusteamedu/symfony-course-2023-02

Add method for use async processing

src/Controller/Api/AddFollowers/v1/Controller.php
@@ -1,0 +1,37 @@
+<?php
+
+namespace App\Controller\Api\AddFollowers\v1;
+
+use App\Manager\UserManager;
+use App\Service\SubscriptionService;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Controller extends AbstractFOSRestController
+{
+    public function __construct(
+        private readonly SubscriptionService $subscriptionService,
+        private readonly UserManager $userManager
+    )
+    {
+    }
+
+    #[Route(path: '/api/v1/add-followers', methods: ['POST'])]
+    #[RequestParam(name: 'userId', requirements: '\d+')]
+    #[RequestParam(name: 'followersLogin')]
+    #[RequestParam(name: 'count', requirements: '\d+')]
+    public function addFollowersAction(int $userId, string $followersLogin, int $count): Response
+    {
+        $user = $this->userManager->getUserById($userId);
+        if ($user !== null) {
+            $createdFollowers = $this->subscriptionService->addFollowers($user, $followersLogin, $count);
+            $view = $this->view(['created' => $createdFollowers], 200);
+        } else {
+            $view = $this->view(['success' => false], 404);
+        }
+
+        return $this->handleView($view);
+    }
+}

src/Service/SubscriptionService.php
@@ -1,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+use App\DTO\ManageUserDTO;
+use App\Entity\User;
+use App\Manager\SubscriptionManager;
+use App\Manager\UserManager;
+
+class SubscriptionService
+{
+    public function __construct(
+        private readonly UserManager $userManager,
+        private readonly SubscriptionManager $subscriptionManager
+    )
+    {
+    }
+
+    public function subscribe(int $authorId, int $followerId): bool
+    {
+        $author = $this->userManager->getUserById($authorId);
+        if (!($author instanceof User)) {
+            return false;
+        }
+        $follower = $this->userManager->getUserById($followerId);
+        if (!($follower instanceof User)) {
+            return false;
+        }
+
+        $this->subscriptionManager->addSubscription($author, $follower);
+
+        return true;
+    }
+
+    public function addFollowers(User $user, string $followerLogin, int $count): int
+    {
+        $createdFollowers = 0;
+        for ($i = 0; $i < $count; $i++) {
+            $followerId = $this->userManager->saveUserFromDTO(
+                new User(),
+                new ManageUserDTO("{$followerLogin}_#$i", $followerLogin, $i, true)
+            );
+            if ($followerId !== null) {
+                $this->subscribe($user->getId(), $followerId);
+                $createdFollowers++;
+            }
+        }
+
+        return $createdFollowers;
+    }
+}

otusteamedu/symfony-course-2024-02

src/Controller/Api/AddFollowers/v1/Controller.php
@@ -1,0 +1,37 @@
+<?php
+
+namespace App\Controller\Api\AddFollowers\v1;
+
+use App\Manager\UserManager;
+use App\Service\SubscriptionService;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Controller extends AbstractFOSRestController
+{
+    public function __construct(
+        private readonly SubscriptionService $subscriptionService,
+        private readonly UserManager $userManager
+    )
+    {
+    }
+
+    #[Route(path: '/api/v1/add-followers', methods: ['POST'])]
+    #[RequestParam(name: 'userId', requirements: '\d+')]
+    #[RequestParam(name: 'followersLogin')]
+    #[RequestParam(name: 'count', requirements: '\d+')]
+    public function addFollowersAction(int $userId, string $followersLogin, int $count): Response
+    {
+        $user = $this->userManager->findUser($userId);
+        if ($user !== null) {
+            $createdFollowers = $this->subscriptionService->addFollowers($user, $followersLogin, $count);
+            $view = $this->view(['created' => $createdFollowers], 200);
+        } else {
+            $view = $this->view(['success' => false], 404);
+        }
+
+        return $this->handleView($view);
+    }
+}

src/Service/SubscriptionService.php
@@ -1,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+use App\DTO\ManageUserDTO;
+use App\Entity\User;
+use App\Manager\SubscriptionManager;
+use App\Manager\UserManager;
+
+class SubscriptionService
+{
+    public function __construct(
+        private readonly UserManager $userManager,
+        private readonly SubscriptionManager $subscriptionManager
+    )
+    {
+    }
+
+    public function subscribe(int $authorId, int $followerId): bool
+    {
+        $author = $this->userManager->findUser($authorId);
+        if (!($author instanceof User)) {
+            return false;
+        }
+        $follower = $this->userManager->findUser($followerId);
+        if (!($follower instanceof User)) {
+            return false;
+        }
+
+        $this->subscriptionManager->addSubscription($author, $follower);
+
+        return true;
+    }
+
+    public function addFollowers(User $user, string $followerLogin, int $count): int
+    {
+        $createdFollowers = 0;
+        for ($i = 0; $i < $count; $i++) {
+            $followerId = $this->userManager->saveUserFromDTO(
+                new User(),
+                new ManageUserDTO("{$followerLogin}_#$i", $followerLogin, $i, true)
+            );
+            if ($followerId !== null) {
+                $this->subscribe($user->getId(), $followerId);
+                $createdFollowers++;
+            }
+        }
+
+        return $createdFollowers;
+    }
+}